8c69dbe2d6f89f1bafa7c300781cf09a65793ea2
José Albert Cruz Almaguer <jalbertcruz@gmail.com>
2023-10-20 16:31:19 +0200

    update

--- lib/dir_nav/__init__.py
+++ lib/dir_nav/__init__.py
@@ -1,42 +1,7 @@
 from attrs import define, field
 import cattrs
 
-
 @define
-class Sprint:
-    id: int = field()
-    state: str = field()
-
-
-@define
-class Sprints:
-    values: list[Sprint] = field()
-
-
-@define
-class Status:
+class Project:
     name: str = field()
-
-
-@define
-class Assignee:
-    emailAddress: str = field()
-
-
-@define
-class Field:
-    status: Status = field()
-    summary: str = field()
-    assignee: Assignee | None = field()
-
-
-@define
-class Issue:
-    id: str = field()
-    key: str = field()
-    fields: Field = field()
-
-
-@define
-class Issues:
-    issues: list[Issue] = field()
+    rootPath: str = field()
