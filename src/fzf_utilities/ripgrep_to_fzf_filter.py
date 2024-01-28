00de166a5868c1dad8e10fe6134e2a57ac756cef
José Albert Cruz Almaguer <jalbertcruz@gmail.com>
2024-01-28 16:42:50 +0100

    Providing previewer support

--- src/fzf_utilities/ripgrep_to_fzf_filter.py
+++ src/fzf_utilities/ripgrep_to_fzf_filter.py
@@ -7,12 +7,16 @@ from attrs import define, field
 import cattrs
 import attrs
 from enum import Enum
+import typer
+from typing_extensions import Annotated
+
 
 # @unique
 class MatchType(Enum):
     BEGIN = "begin"
     MATCH = "match"
     END = "end"
+
 
 @define
 class Path:
@@ -27,11 +31,13 @@ class Line:
         validator=attrs.validators.instance_of(str)
     )
 
+
 @define
 class Submatch:
     start: int = field(
         validator=attrs.validators.instance_of(int)
     )
+
 
 @define
 class Match:
@@ -47,6 +53,7 @@ class Match:
     submatches: list[Submatch] = field(
     )
 
+
 @define
 class RGLine:
     type: MatchType = field(
@@ -55,6 +62,7 @@ class RGLine:
     data: Match = field(
         validator=attrs.validators.instance_of(Match)
     )
+
     def row(self):
         return self.data.line_number
 
@@ -68,23 +76,26 @@ class RGLine:
         return self.data.submatches[0].start + 1
 
 
-def main():
+def main(
+        rows_before: Annotated[int, typer.Option("--rb")],
+        lines_count: Annotated[int, typer.Option("--lc")],
+):
     lines = sys.stdin.readlines()
     if len(lines) == 0:
         sys.exit(0)
-    result=[]
+    result = []
     for line in lines:
         value = json.loads(line)
         try:
             self = cattrs.structure(value, RGLine)
             # TODO: remove magic numbers
-            row_start = max(self.row() - 3, 1)
-            lines_count = 4
+            row_start = max(self.row() - rows_before, 1)
             result.append(f"{self.file()}:{self.row()}:{self.column()}:{row_start}:{lines_count}:{self.text()}")
         except Exception as e:
             pass
 
     print("\n".join(result))
 
+
 if __name__ == "__main__":
-    main()
+    typer.run(main)
