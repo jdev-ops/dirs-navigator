0bddf38e0554bf5ec5333ad43c16390844f65678
José Albert Cruz Almaguer <jalbertcruz@gmail.com>
2024-02-15 21:22:21 +0100

    update

--- src/dirs_navigator/navigator.py
+++ src/dirs_navigator/navigator.py
@@ -17,14 +17,15 @@ elif Path(".env.local").is_file():
 else:
     config = decouple_config
 
+
 def sel_env():
     import glob
 
     options = glob.glob(f"{os.environ['HOME']}/.navigator/*.yaml")
     values = {}
     for o in options:
-        i=o.rfind("/")
-        values[o[i+1:-5]]= o
+        i = o.rfind("/")
+        values[o[i + 1:-5]] = o
     my_env = os.environ.copy()
     my_env["GUM_FILTER_PLACEHOLDER"] = f"Choose an environment:"
     result = subprocess.run(
@@ -32,6 +33,7 @@ def sel_env():
     )
     open(f"{os.environ['HOME']}/.navigator/.environment", "w").write(result.stdout.strip())
     return 0
+
 
 def choose_destination():
     from yaml import load, dump
@@ -41,16 +43,20 @@ def choose_destination():
         from yaml import Loader, Dumper
 
     environment = open(f"{os.environ['HOME']}/.navigator/.environment").read()
-    doc=load(open(f"{os.environ['HOME']}/.navigator/{environment}.yaml"), Loader=Loader)
+    doc = load(open(f"{os.environ['HOME']}/.navigator/{environment}.yaml"), Loader=Loader)
     all_paths = cattrs.structure(doc, list[Project])
-    values = { p.name: p.rootPath for p in all_paths}
+    values = {p.name: p.rootPath for p in all_paths}
     my_env = os.environ.copy()
     my_env["GUM_FILTER_PLACEHOLDER"] = f"Choose an environment:"
     result = subprocess.run(
         ["gum", "filter"], input="\n".join(values.keys()), stdout=subprocess.PIPE, text=True, env=my_env
     )
-    selected_path = values[result.stdout.strip()]
-    if selected_path.startswith("~"):
-        selected_path = selected_path.replace("~", os.environ["HOME"])
-    print(selected_path)
-    return 0
+    sel = result.stdout.strip()
+    if sel:
+        selected_path = values[sel]
+        if selected_path.startswith("~"):
+            selected_path = selected_path.replace("~", os.environ["HOME"])
+        print(selected_path)
+        return 0
+    else:
+        return 1
